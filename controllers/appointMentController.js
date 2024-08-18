ebe9a026455205258ae09c5e0d6558f9f9c04b93
ammmar <ammaraslam7164@gmail.com>
2024-08-18 14:14:40 +0600

    get and post mongoose setted 100

--- controllers/appointMentController.js
+++ controllers/appointMentController.js
@@ -1,7 +1,38 @@
 const appointment = require("../models/appointMentModel");
 
+exports.getAllAppointment = async (req, res) => {
+  try {
+    const newAppointment = await appointment.find({});
+    res.status(201).json({
+      message: "Appointment fetched successfully",
+      appointment: newAppointment
+    });
+  } catch (error) {
+    console.error("fetching Error:", error.errors);
+    res
+      .status(500)
+      .json({ message: "Error getting appointment", error: error.message });
+  }
+};
+
 exports.saveAppointment = async (req, res) => {
-    const {
+  const {
+    firstname,
+    lastname,
+    email,
+    phone,
+    nic,
+    date,
+    gender,
+    birthday,
+    birthtime,
+    department,
+    doctor,
+    address,
+  } = req.body;
+
+  try {
+    const newAppointment = await appointment.create({
       firstname,
       lastname,
       email,
@@ -14,36 +45,17 @@ exports.saveAppointment = async (req, res) => {
       department,
       doctor,
       address,
-    } = req.body;
-  
-    try {
-      const newAppointment = await appointment.create({
-        firstname,
-        lastname,
-        email,
-        phone,
-        nic,
-        date,
-        gender,
-        birthday,
-        birthtime,
-        department,
-        doctor,
-        address,
-      });
-  
-      console.log(newAppointment);
-      res
-        .status(201)
-        .json({
-          message: "Appointment saved successfully",
-          appointment: newAppointment,
-        });
-    } catch (error) {
-      console.error("Validation Error:", error.errors);
-      res
-        .status(500)
-        .json({ message: "Error saving appointment", error: error.message });
-    }
-  }
+    });
 
+    console.log(newAppointment);
+    res.status(201).json({
+      message: "Appointment saved successfully",
+      appointment: newAppointment,
+    });
+  } catch (error) {
+    console.error("Validation Error:", error.errors);
+    res
+      .status(500)
+      .json({ message: "Error saving appointment", error: error.message });
+  }
+};
--- routes/routeAppointMent.js
+++ routes/routeAppointMent.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { saveAppointment } = require('../controllers/appointMentController');
+const { saveAppointment, getAllAppointment } = require('../controllers/appointMentController');
 
 const router = express.Router();
 
 router.post("/saveAppointment",saveAppointment);
+router.get("/getAllAppointment",getAllAppointment);
 
 
 module.exports = router
